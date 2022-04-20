263fb7ca60d637472d3ae4516d8c647039af09fe
Michael Bishop <michaelabishop1988@gmail.com>
2022-04-20 16:18:11 +0100

    commited nothing

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,10 +7,10 @@ const Home = ({
         <div>
             {cat.map((item, index) => {
                 return (
-                    <>
-                        <p key={index}>{item.id}</p>
-                        <img src={item.url} alt="random" />
-                    </>
+                    <div className="cat-div">
+                        <p className="cat-div-name" key={index}>{item.id}</p>
+                        <img className="cat-div-img" src={item.url} alt="random" />
+                    </div>
                 )
             })}
         </div>
